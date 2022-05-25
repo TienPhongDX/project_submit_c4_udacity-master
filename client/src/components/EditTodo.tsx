3cc0dcbd39590e6b9a96d1e46a55a8148e52898a
Nguyen Tien Phong <42340547+TienPhongDX@users.noreply.github.com>
2022-05-25 14:48:23 +0700

    update frontend

--- client/src/components/EditTodo.tsx
+++ client/src/components/EditTodo.tsx
@@ -88,6 +88,7 @@ export class EditTodo extends React.PureComponent<
               placeholder="Image to upload"
               onChange={this.handleFileChange}
             />
+            <img alt="" />
           </Form.Field>
 
           {this.renderButton()}
@@ -102,6 +103,7 @@ export class EditTodo extends React.PureComponent<
       <div>
         {this.state.uploadState === UploadState.FetchingPresignedUrl && <p>Uploading image metadata</p>}
         {this.state.uploadState === UploadState.UploadingFile && <p>Uploading file</p>}
+        
         <Button
           loading={this.state.uploadState !== UploadState.NoUpload}
           type="submit"
